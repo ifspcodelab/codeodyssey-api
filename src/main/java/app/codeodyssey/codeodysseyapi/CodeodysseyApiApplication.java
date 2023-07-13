08ae146f3671a0d8e6aeaf72c25be7759019391d
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-13 12:44:55 -0300

    initial commit

---
+++ src/main/java/app/codeodyssey/codeodysseyapi/CodeodysseyApiApplication.java
@@ -0,0 +1,13 @@
+package app.codeodyssey.codeodysseyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeodysseyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeodysseyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/codeodyssey/codeodysseyapi/CodeodysseyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.codeodyssey.codeodysseyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeodysseyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
