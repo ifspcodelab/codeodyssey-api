6bbe892a1bc8e9328638845a65f1510e1773e54c
Domingos Latorre <domingos.latorre@gmail.com>
2023-07-14 11:39:15 -0300

    chore: add test task to code quality workflow

--- src/test/java/app/codeodyssey/codeodysseyapi/CodeodysseyApiApplicationTests.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/CodeodysseyApiApplicationTests.java
@@ -1,9 +1,7 @@
 package app.codeodyssey.codeodysseyapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CodeodysseyApiApplicationTests {
 
     @Test
