aaf1e6a904cc03579ebc81b4e1d48408f250a0ba
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-18 12:18:30 -0300

    fix: repository annotation on invitation repository interface

--- src/main/java/app/codeodyssey/codeodysseyapi/invitation/data/InvitationRepository.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/invitation/data/InvitationRepository.java
@@ -2,5 +2,7 @@ package app.codeodyssey.codeodysseyapi.invitation.data;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InvitationRepository extends JpaRepository<Invitation, UUID> {}
