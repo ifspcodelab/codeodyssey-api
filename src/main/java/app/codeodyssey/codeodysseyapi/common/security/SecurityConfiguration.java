00d97865f794a5d6cfe0fe6e4e1dcad62c15ffc8
kevindotklein <kevindotklein@gmail.com>
2023-07-18 15:35:16 -0300

    feat: authorization

--- src/main/java/app/codeodyssey/codeodysseyapi/common/security/SecurityConfiguration.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/common/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,15 +15,24 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     private static final String[] NO_AUTH_REQUIRED = {
-            "/api/v1/**",
+            "/api/v1/login",
             "/swagger-ui/**",
-            "/v3/api-docs/**"
+            "/v3/api-docs/**",
+    };
+
+    private static final String[] STUDENTS_ALLOWED = {
+
+    };
+
+    private static final String[] PROFESSORS_ALLOWED = {
+
     };
 
     @Bean
@@ -31,6 +41,9 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(NO_AUTH_REQUIRED).permitAll()
+                        .requestMatchers(STUDENTS_ALLOWED).hasAnyAuthority("STUDENT")
+                        .requestMatchers(PROFESSORS_ALLOWED).hasAnyAuthority("PROFESSOR")
+                        .requestMatchers("/**").hasAnyAuthority("ADMIN")
                         .anyRequest()
                         .authenticated()
                 )
