7a4e0a72e053e974784f3aaf931ba4b9320dc1cd
kevindotklein <kevindotklein@gmail.com>
2023-07-18 14:38:55 -0300

    docs: login documentation

--- src/main/java/app/codeodyssey/codeodysseyapi/user/api/LoginUserEndpoint.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/user/api/LoginUserEndpoint.java
@@ -2,8 +2,15 @@ package app.codeodyssey.codeodysseyapi.user.api;
 
 import app.codeodyssey.codeodysseyapi.token.api.TokenResponse;
 import app.codeodyssey.codeodysseyapi.token.service.GetTokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,10 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Tag(name = "Login", description = "Login API endpoint")
 public class LoginUserEndpoint {
 
     private final GetTokenService getTokenService;
 
+    @Operation(
+            summary = "Log in into the platform",
+            description = "Returns a JWT",
+            tags = {"Users"})
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = TokenResponse.class),
+                                    mediaType = "application/json")
+                    }),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = {
+                            @Content(
+                                    schema = @Schema(implementation = ProblemDetail.class),
+                                    mediaType = "application/json")
+                    })
+    })
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody UserRequest userRequest){
         return new ResponseEntity<>(getTokenService.execute(userRequest), HttpStatus.OK);
