201cc008fe0be391a0ba7655d0c48c52661273a2
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-19 17:47:21 -0300

    test: find all by order by name and end date returns an empty list

--- src/test/java/app/codeodyssey/codeodysseyapi/course/data/CourseRepositoryTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/course/data/CourseRepositoryTest.java
@@ -1,8 +1,13 @@
 package app.codeodyssey.codeodysseyapi.course.data;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class CourseRepositoryTest {
@@ -11,4 +16,22 @@ public class CourseRepositoryTest {
 
     @Autowired
     private CourseRepository courseRepository;
+
+    /**
+     * TODO: findAllOrderByNameAscEndDateAsc();
+     * case 1: returns an empty list
+     * case 2: returns a list with 1 element
+     * case 3: returns a list with N elements
+     * case 4: returns a list with element named A before element B
+     * case 5: returns a list with element named AA and end date january 1st before element AA january 2nd
+     */
+    @Test
+    void findAllByOrderByNameAscEndDateAsc_givenEmptyDatabase_returnEmptyList() {
+        testEntityManager.flush();
+
+        List<Course> courseList = courseRepository.findAllByOrderByNameAscEndDateAsc();
+        System.out.println(courseList.toString());
+
+        assertThat(courseList).isEmpty();
+    }
 }
