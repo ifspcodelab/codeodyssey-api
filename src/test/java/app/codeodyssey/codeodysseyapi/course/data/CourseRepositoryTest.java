04b4d2331f56b15b1e68aa2cd3d15fc8e9e2825d
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-20 15:40:32 -0300

    test: find all by order by name asc end date asc returns a list ordered by name and end date

--- src/test/java/app/codeodyssey/codeodysseyapi/course/data/CourseRepositoryTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/course/data/CourseRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -101,5 +102,34 @@ public class CourseRepositoryTest {
         assertThat(courseList).isNotEmpty();
         assertThat(courseList).hasSize(2);
         assertThat(courseList).containsExactly(courseA, courseB);
+    }
+
+    @Test
+    @DisplayName("findAllByOrderByNameAscEndDateAsc() returns a list ordered by course name ascending and end date ascending when the course table has many stored rows")
+    void findAllByOrderByNameAscEndDateAsc_givenManyStoredRows_returnsListOrderedByNameAscEndDateAsc() {
+        var user = UserFactory.sampleUserProfessor();
+        var courseA1 = CourseFactory.sampleCourseWithProfessor(user);
+        var courseA2 = CourseFactory.sampleCourseWithProfessor(user);
+        var courseB = CourseFactory.sampleCourseWithProfessor(user);
+        courseA1.setName("Spring Cloud");
+        courseA1.setSlug("spring-cloud");
+        courseA1.setEndDate(LocalDate.of(2023, 7, 17));
+        courseA2.setName("Spring Cloud");
+        courseA2.setSlug("spring-cloud-second-class");
+        courseA2.setEndDate(LocalDate.of(2023, 7, 18));
+        courseB.setName("Spring Security");
+        courseB.setSlug("spring-security");
+        courseB.setEndDate(LocalDate.of(2023, 7, 17));
+        testEntityManager.persistAndFlush(user);
+        testEntityManager.persistAndFlush(courseB);
+        testEntityManager.persistAndFlush(courseA2);
+        testEntityManager.persistAndFlush(courseA1);
+
+        List<Course> courseList = courseRepository.findAllByOrderByNameAscEndDateAsc();
+        System.out.println(courseList.toString());
+
+        assertThat(courseList).isNotEmpty();
+        assertThat(courseList).hasSize(3);
+        assertThat(courseList).containsExactly(courseA1, courseA2, courseB);
     }
 }
