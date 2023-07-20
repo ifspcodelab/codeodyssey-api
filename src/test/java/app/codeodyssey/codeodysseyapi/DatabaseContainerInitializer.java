d5af928e21c32e100343d4588d40b34bfdebd4fe
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-20 13:33:07 -0300

    chore: testcontainers configuration and initializer class

---
+++ src/test/java/app/codeodyssey/codeodysseyapi/DatabaseContainerInitializer.java
@@ -0,0 +1,23 @@
+package app.codeodyssey.codeodysseyapi;
+
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public class DatabaseContainerInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    private static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    static {
+        postgreSQLContainer.start();
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        TestPropertyValues.of(
+                "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                "spring.datasource.password=" + postgreSQLContainer.getPassword()
+        ).applyTo(applicationContext.getEnvironment());
+    }
+}
