addca9687ed46f6c6775eb479d0b81cd68d41565
Raquel Tsuji <raqueltsuji@gmail.com>
2023-07-23 23:22:18 -0300

    fix: change user mapper and resource name

--- src/main/java/app/codeodyssey/codeodysseyapi/common/exceptions/ResourceName.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/common/exceptions/ResourceName.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResourceName {
-    COURSE("Course");
+    COURSE("Course"),
+    USER("User");
 
     private final String name;
 }
--- src/main/java/app/codeodyssey/codeodysseyapi/user/UserMapper.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/user/UserMapper.java
@@ -2,9 +2,7 @@ package app.codeodyssey.codeodysseyapi.user;
 
 import org.mapstruct.Mapper;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    List<UserResponse> to(List<User> user);
+    UserResponse to(User user);
 }
