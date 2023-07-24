78ae98cf459efd8d06e8d597d036256872b334f0
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-24 17:12:23 -0300

    test: get courses service returns unauthorized

---
+++ src/test/java/app/codeodyssey/codeodysseyapi/course/service/GetCoursesServiceTest.java
@@ -0,0 +1,41 @@
+package app.codeodyssey.codeodysseyapi.course.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import app.codeodyssey.codeodysseyapi.DatabaseContainerInitializer;
+import app.codeodyssey.codeodysseyapi.common.exception.UnauthorizedAccessException;
+import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
+import app.codeodyssey.codeodysseyapi.user.util.UserFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@DisplayName("Get Courses Service tests")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = {DatabaseContainerInitializer.class})
+public class GetCoursesServiceTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private GetCoursesService getCoursesService;
+
+    @Test
+    @DisplayName("getCoursesService returns UnauthorizedException when an user is not admin")
+    void getCoursesService_givenNonAdmin_returnsUnauthorized() {
+        var user = UserFactory.sampleUserStudent();
+        userRepository.save(user);
+
+        RuntimeException exception =
+                (RuntimeException) catchThrowable(() -> getCoursesService.execute(user.getEmail()));
+
+        assertThat(exception).isInstanceOf(UnauthorizedAccessException.class);
+        UnauthorizedAccessException unauthorizedAccessException = (UnauthorizedAccessException) exception;
+        assertThat(unauthorizedAccessException.getId()).isEqualTo(user.getId());
+    }
+}
