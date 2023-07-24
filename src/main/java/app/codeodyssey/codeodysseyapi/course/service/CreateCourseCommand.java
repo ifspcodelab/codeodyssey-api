70e51ec01b893c871a424c019067d19ec65884b4
Raquel Tsuji <raqueltsuji@gmail.com>
2023-07-23 21:25:52 -0300

    fix: change course table endDate constraint

--- src/main/java/app/codeodyssey/codeodysseyapi/course/service/CreateCourseCommand.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/course/service/CreateCourseCommand.java
@@ -4,9 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.util.UUID;
 
 public record CreateCourseCommand(
         @NotNull @NotBlank @Size(min = 1, max = 255) String name,
--- src/main/resources/db/migration/V2__create_courses_table.sql
+++ src/main/resources/db/migration/V2__create_courses_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE courses(
     name VARCHAR NOT NULL,
     slug VARCHAR NOT NULL UNIQUE,
     start_date DATE NOT NULL,
-    end_date DATE,
+    end_date DATE NOT NULL,
     created_at TIMESTAMPTZ NOT NULL,
     professor_id UUID NOT NULL,
     CONSTRAINT courses_pk PRIMARY KEY (id),
