aaba730a99fa15e7471c978e43de46bae81465b3
Sergio <sergioadm120@gmail.com>
2023-07-25 14:24:00 -0300

    test: add UserCleanupService test

---
+++ src/test/java/app/codeodyssey/codeodysseyapi/UserCleanupServiceTest.java
@@ -0,0 +1,54 @@
+package app.codeodyssey.codeodysseyapi;
+
+import app.codeodyssey.codeodysseyapi.user.data.User;
+import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
+import app.codeodyssey.codeodysseyapi.user.service.UserCleanupService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = {DatabaseContainerInitializer.class})
+@Testcontainers
+public class UserCleanupServiceTest {
+    @Autowired
+    private UserCleanupService userCleanupService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void testExecute_cleanNonValidatedUser() {
+        User user = new User("Sergio", "sergio@example.com", "password");
+        user.setCreatedAt(user.getCreatedAt().minus(180, ChronoUnit.SECONDS));
+        userRepository.save(user);
+
+        userCleanupService.cleanupUser();
+
+        assertThat(userRepository.getUserByEmail(user.getEmail())).isNull();
+
+    }
+
+    @Test
+    void testExecute_dontCleanValidUsers() {
+        User user = new User("Sergio", "sergio@example.com", "password");
+        userRepository.save(user);
+
+        userCleanupService.cleanupUser();
+
+        User receivedUser = userRepository.getUserByEmail(user.getEmail());
+
+        assertThat(userRepository.getUserByEmail(user.getEmail())).isNotNull();
+        assertEquals(receivedUser.getEmail(), user.getEmail());
+    }
+}
