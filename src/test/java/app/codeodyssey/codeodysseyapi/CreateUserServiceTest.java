e6e2b99c3189ef4bb545bfd621bb2fb3173e40d9
Sergio <sergioadm120@gmail.com>
2023-07-26 11:30:22 -0300

    refractor: change test names in the CreatUserServiceTest

--- src/test/java/app/codeodyssey/codeodysseyapi/CreateUserServiceTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/CreateUserServiceTest.java
@@ -6,6 +6,7 @@ import app.codeodyssey.codeodysseyapi.user.data.User;
 import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
 import app.codeodyssey.codeodysseyapi.user.service.CreateUserCommand;
 import app.codeodyssey.codeodysseyapi.user.service.CreateUserService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -14,9 +15,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+@DisplayName("test for the CreateUserService")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(initializers = {DatabaseContainerInitializer.class})
 @Testcontainers
@@ -28,17 +31,22 @@ public class CreateUserServiceTest {
     private CreateUserService createUserService;
 
     @Test
-    void testExecute_CreateUser_Success() {
+    @DisplayName("create and save a user")
+    void execute_givenValidUser_returnsUserResponse() {
         CreateUserCommand userCommand = new CreateUserCommand("sergio", "gabriel@example.com",
                 "password123");
 
         UserResponse user = createUserService.execute(userCommand);
 
+        User foundUser = userRepository.getUserByEmail(userCommand.email());
+
         assertThat(user).isNotNull();
+        assertEquals(user.id(), foundUser.getId());
     }
 
     @Test
-    void testExecute_CreateUser_UserAlreadyExists() {
+    @DisplayName("throws exception due the attempt to create a duplicated user")
+    void execute_givenDuplicateUser_throwsException() {
         CreateUserCommand command = new CreateUserCommand("Sergio", "sergio@example.com", "password#123");
 
         userRepository.save(new User("sergio@example.com", "Sergio", "password"));
