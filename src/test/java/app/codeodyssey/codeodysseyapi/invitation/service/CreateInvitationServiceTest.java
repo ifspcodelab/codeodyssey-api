0035f06b64424b344ea3511c2d8ba80881907ccc
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-27 14:11:21 -0300

    test: create invitation service returns unauthorized

--- src/test/java/app/codeodyssey/codeodysseyapi/invitation/service/CreateInvitationServiceTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/invitation/service/CreateInvitationServiceTest.java
@@ -1,11 +1,13 @@
 package app.codeodyssey.codeodysseyapi.invitation.service;
 
 import app.codeodyssey.codeodysseyapi.DatabaseContainerInitializer;
+import app.codeodyssey.codeodysseyapi.common.exception.UnauthorizedAccessException;
 import app.codeodyssey.codeodysseyapi.course.data.CourseRepository;
 import app.codeodyssey.codeodysseyapi.course.util.CourseFactory;
 import app.codeodyssey.codeodysseyapi.invitation.api.InvitationResponse;
 import app.codeodyssey.codeodysseyapi.invitation.data.InvitationRepository;
 import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
+import app.codeodyssey.codeodysseyapi.user.util.UserFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,8 +17,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
@@ -60,5 +64,25 @@ public class CreateInvitationServiceTest {
         assertThat(invitation).isInstanceOf(InvitationResponse.class);
         assertThat(invitation.expirationDate()).isEqualTo(command.expirationDate());
         assertThat(invitation.link()).isEqualTo("/invites/%s".formatted(invitation.id()));
+    }
+
+    @Test
+    @DisplayName("createInvitationService given non professor user returns unauthorized")
+    void createInvitationService_givenNonProfessor_returnsUnauthorized() {
+        var course = CourseFactory.sampleCourse();
+        var professor = course.getProfessor();
+        var user = UserFactory.sampleUserStudent();
+        var command = new InvitationCreateCommand(LocalDate.now());
+        userRepository.saveAll(List.of(professor, user));
+        courseRepository.save(course);
+
+        var serviceThrowable = (RuntimeException) catchThrowable(() ->
+                createInvitationService.execute(command, course.getId(), user.getEmail())
+        );
+
+        assertThat(serviceThrowable).isNotNull();
+        assertThat(serviceThrowable).isInstanceOf(UnauthorizedAccessException.class);
+        UnauthorizedAccessException unauthorizedAccessException = (UnauthorizedAccessException) serviceThrowable;
+        assertThat(unauthorizedAccessException.getId()).isEqualTo(user.getId());
     }
 }
