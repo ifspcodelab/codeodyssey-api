01b449f499b1cca4dc105e094e110ac2374ff2c2
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-27 17:26:14 -0300

    docs: add create invitation endpoint documentation

--- src/main/java/app/codeodyssey/codeodysseyapi/invitation/api/CreateInvitationEndpoint.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/invitation/api/CreateInvitationEndpoint.java
@@ -2,6 +2,12 @@ package app.codeodyssey.codeodysseyapi.invitation.api;
 
 import app.codeodyssey.codeodysseyapi.invitation.service.CreateInvitationService;
 import app.codeodyssey.codeodysseyapi.invitation.service.InvitationCreateCommand;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
@@ -15,16 +21,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("api/v1/courses")
 @AllArgsConstructor
-@Tag(name = "Invitation", description = "Invitation API endpoint")
+@Tag(name = "Invitations", description = "Invitation API endpoint")
 public class CreateInvitationEndpoint {
     private final CreateInvitationService createInvitationService;
 
+    @Operation(
+            summary = "Create invitations.",
+            description = "Returns an invitation of a course given said course's id.",
+            tags = {"Invitations"})
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "201",
+                content = {
+                    @Content(
+                            array = @ArraySchema(schema = @Schema(implementation = InvitationResponse.class)),
+                            mediaType = "application/json")
+                })
+    })
     @PostMapping("{courseId}/invitations")
     public ResponseEntity<InvitationResponse> post(
             @PathVariable @Valid UUID courseId,
             @RequestBody @Valid InvitationCreateCommand command,
             Authentication authentication) {
-        System.out.println("hey @ controller");
         var userDetails = (UserDetails) authentication.getPrincipal();
         var username = userDetails.getUsername();
         return new ResponseEntity<>(createInvitationService.execute(command, courseId, username), HttpStatus.CREATED);
