d38f649a488309e6e0adcf79b5936e9d33829182
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-28 17:19:28 -0300

    docs: create enrollment endpoint

--- src/main/java/app/codeodyssey/codeodysseyapi/enrollment/api/CreateEnrollmentEndpoint.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/enrollment/api/CreateEnrollmentEndpoint.java
@@ -2,6 +2,12 @@ package app.codeodyssey.codeodysseyapi.enrollment.api;
 
 import app.codeodyssey.codeodysseyapi.enrollment.service.CreateEnrollmentService;
 import app.codeodyssey.codeodysseyapi.enrollment.service.EnrollmentResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.UUID;
@@ -22,6 +28,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class CreateEnrollmentEndpoint {
     private final CreateEnrollmentService createEnrollmentService;
 
+    @Operation(
+            summary = "Create enrollments.",
+            description = "Returns an enrollment of a student on a course, given an invitation id of said course.",
+            tags = {"Enrollments"})
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "201",
+                content = {
+                    @Content(
+                            array = @ArraySchema(schema = @Schema(implementation = EnrollmentResponse.class)),
+                            mediaType = "application/json")
+                })
+    })
     @PostMapping("{invitationId}/enrollments")
     public ResponseEntity<EnrollmentResponse> post(
             @PathVariable @Valid UUID invitationId, Authentication authentication) {
