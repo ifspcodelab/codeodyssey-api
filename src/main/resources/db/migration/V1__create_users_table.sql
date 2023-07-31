508e72956f0d95aef274fe79f0a389ecf1376bff
Lucas Gabriel <lucasogjc@gmail.com>
2023-07-31 15:33:28 -0300

    feat: character limit on attributes of create users table migration

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users(
     id UUID,
-    email VARCHAR NOT NULL UNIQUE,
-    name VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
+    email VARCHAR(350) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    password CHAR(60) NOT NULL,
     role VARCHAR NOT NULL,
     created_at TIMESTAMP NOT NULL,
     is_validated BOOLEAN NOT NULL,
