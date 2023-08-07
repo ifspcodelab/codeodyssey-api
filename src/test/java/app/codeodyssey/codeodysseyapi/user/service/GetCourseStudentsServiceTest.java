ee57cb3a2a86e996c86b8de2009a20bc516f9610
Raquel Tsuji <raqueltsuji@gmail.com>
2023-08-07 14:57:13 -0300

    refactor: erase duplicated code

--- src/test/java/app/codeodyssey/codeodysseyapi/user/service/GetCourseStudentsServiceTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/user/service/GetCourseStudentsServiceTest.java
@@ -4,10 +4,13 @@ import app.codeodyssey.codeodysseyapi.DatabaseContainerInitializer;
 import app.codeodyssey.codeodysseyapi.common.exception.ForbiddenAccessException;
 import app.codeodyssey.codeodysseyapi.common.exception.ResourceNotFoundException;
 import app.codeodyssey.codeodysseyapi.common.exception.ViolationException;
+import app.codeodyssey.codeodysseyapi.course.data.Course;
 import app.codeodyssey.codeodysseyapi.course.data.CourseRepository;
 import app.codeodyssey.codeodysseyapi.course.util.CourseFactory;
+import app.codeodyssey.codeodysseyapi.enrollment.data.Enrollment;
 import app.codeodyssey.codeodysseyapi.enrollment.data.EnrollmentRepository;
 import app.codeodyssey.codeodysseyapi.enrollment.util.EnrollmentFactory;
+import app.codeodyssey.codeodysseyapi.invitation.data.Invitation;
 import app.codeodyssey.codeodysseyapi.invitation.data.InvitationRepository;
 import app.codeodyssey.codeodysseyapi.invitation.util.InvitationFactory;
 import app.codeodyssey.codeodysseyapi.user.data.User;
@@ -15,6 +18,7 @@ import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
 import app.codeodyssey.codeodysseyapi.user.util.UserFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,8 +51,28 @@ public class GetCourseStudentsServiceTest {
     @Autowired
     private EnrollmentRepository enrollmentRepository;
 
-    @AfterEach
+    User professor, student;
+    Course course;
+    Invitation invitation;
+    Enrollment enrollment;
+
+    @BeforeEach
     void setUp() {
+        professor = UserFactory.createValidProfessor();
+        course = CourseFactory.createValidCourseWithProfessor(professor);
+        invitation = InvitationFactory.sampleInvitationWithCourse(course);
+        student = UserFactory.sampleUserStudent();
+        enrollment = EnrollmentFactory.sampleEnrollment(invitation, student);
+
+        userRepository.save(professor);
+        userRepository.save(student);
+        courseRepository.save(course);
+        invitationRepository.save(invitation);
+        enrollmentRepository.save(enrollment);
+    }
+
+    @AfterEach
+    void tearDown() {
         enrollmentRepository.deleteAll();
         invitationRepository.deleteAll();
         courseRepository.deleteAll();
@@ -58,17 +82,6 @@ public class GetCourseStudentsServiceTest {
     @Test
     @DisplayName("returns a user list when given a valid professor id and course slug")
     void getCourseStudentsService_givenProfessorIdAndSlug_returnsList() {
-        var professor = UserFactory.createValidProfessor();
-        var course = CourseFactory.createValidCourseWithProfessor(professor);
-        var invitation = InvitationFactory.sampleInvitationWithCourse(course);
-        var student = UserFactory.createValidUser();
-        var enrollment = EnrollmentFactory.sampleEnrollment(invitation, student);
-        userRepository.save(professor);
-        userRepository.save(student);
-        courseRepository.save(course);
-        invitationRepository.save(invitation);
-        enrollmentRepository.save(enrollment);
-
         List<User> userList = userRepository.findUsersByCourseIdOrderByName(course.getId());
         assertThat(userList).isNotEmpty();
     }
@@ -76,17 +89,6 @@ public class GetCourseStudentsServiceTest {
     @Test
     @DisplayName("returns exception when given a an invalid user id")
     void getCourseStudentsService_givenInvalidUserId_returns404NotFound() {
-        var professor = UserFactory.createValidProfessor();
-        var course = CourseFactory.createValidCourseWithProfessor(professor);
-        var invitation = InvitationFactory.sampleInvitationWithCourse(course);
-        var student = UserFactory.createValidUser();
-        var enrollment = EnrollmentFactory.sampleEnrollment(invitation, student);
-        userRepository.save(professor);
-        userRepository.save(student);
-        courseRepository.save(course);
-        invitationRepository.save(invitation);
-        enrollmentRepository.save(enrollment);
-
         Assertions.assertThatExceptionOfType(ResourceNotFoundException.class)
                 .isThrownBy(() -> getCourseStudentsService.execute(UUID.randomUUID(), course.getSlug()));
     }
@@ -95,17 +97,7 @@ public class GetCourseStudentsServiceTest {
     @DisplayName("returns exception when given a an invalid professor role")
     void getCourseStudentsService_givenInvalidProfessorRole_returns403Forbidden() {
         var user = UserFactory.createValidUser();
-        var professor = UserFactory.createValidProfessor();
-        var course = CourseFactory.createValidCourseWithProfessor(professor);
-        var invitation = InvitationFactory.sampleInvitationWithCourse(course);
-        var student = UserFactory.sampleUserStudent();
-        var enrollment = EnrollmentFactory.sampleEnrollment(invitation, student);
         userRepository.save(user);
-        userRepository.save(professor);
-        userRepository.save(student);
-        courseRepository.save(course);
-        invitationRepository.save(invitation);
-        enrollmentRepository.save(enrollment);
 
         Assertions.assertThatExceptionOfType(ForbiddenAccessException.class)
                 .isThrownBy(() -> getCourseStudentsService.execute(user.getId(), course.getSlug()));
@@ -114,20 +106,10 @@ public class GetCourseStudentsServiceTest {
     @Test
     @DisplayName("returns exception when given a an invalid professor id and course slug")
     void getCourseStudentsService_givenInvalidSlugAndProfessorId_returns401Conflict() {
-        var professor = UserFactory.createValidProfessor();
         var professorB = UserFactory.sampleUserProfessorB();
-        var course = CourseFactory.createValidCourseWithProfessor(professorB);
-        var invitation = InvitationFactory.sampleInvitationWithCourse(course);
-        var student = UserFactory.createValidUser();
-        var enrollment = EnrollmentFactory.sampleEnrollment(invitation, student);
-        userRepository.save(professor);
         userRepository.save(professorB);
-        userRepository.save(student);
-        courseRepository.save(course);
-        invitationRepository.save(invitation);
-        enrollmentRepository.save(enrollment);
 
         Assertions.assertThatExceptionOfType(ViolationException.class)
-                .isThrownBy(() -> getCourseStudentsService.execute(professor.getId(), course.getSlug()));
+                .isThrownBy(() -> getCourseStudentsService.execute(professorB.getId(), course.getSlug()));
     }
 }
