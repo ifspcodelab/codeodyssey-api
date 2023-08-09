0746ef5ea196c8b6b0ff04aa1b299b47016d750c
Raquel Tsuji <raqueltsuji@gmail.com>
2023-08-08 23:08:55 -0300

    test: change get course students endpoint test to e2e directory

--- src/test/java/app/codeodyssey/codeodysseyapi/user/e2e/GetCourseStudentsEndToEndTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/user/e2e/GetCourseStudentsEndToEndTest.java
@@ -1,4 +1,4 @@
-package app.codeodyssey.codeodysseyapi.user.api;
+package app.codeodyssey.codeodysseyapi.user.e2e;
 
 import app.codeodyssey.codeodysseyapi.DatabaseContainerInitializer;
 import app.codeodyssey.codeodysseyapi.course.data.Course;
