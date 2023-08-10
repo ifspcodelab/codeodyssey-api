a2b37ebed48019467d37f214d0c38e63424c60ec
Sergio <sergioadm120@gmail.com>
2023-08-10 12:05:06 -0300

    try to make Greenmail works

--- src/test/java/app/codeodyssey/codeodysseyapi/user/service/SendEmailServiceTest.java
+++ src/test/java/app/codeodyssey/codeodysseyapi/user/service/SendEmailServiceTest.java
@@ -3,11 +3,11 @@ package app.codeodyssey.codeodysseyapi.user.service;
 import app.codeodyssey.codeodysseyapi.DatabaseContainerInitializer;
 import app.codeodyssey.codeodysseyapi.user.data.User;
 import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
-import com.icegreen.greenmail.configuration.GreenMailConfiguration;
-import com.icegreen.greenmail.junit5.GreenMailExtension;
-import com.icegreen.greenmail.util.ServerSetupTest;
+import com.icegreen.greenmail.spring.GreenMailBean;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,13 +15,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+
 
 import java.util.Optional;
 import static org.mockito.Mockito.verify;
@@ -31,7 +27,7 @@ import static org.mockito.Mockito.any;
 @DisplayName("test for the SendEmailService")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(initializers = {DatabaseContainerInitializer.class})
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @Testcontainers
 public class SendEmailServiceTest {
     @Autowired
@@ -42,31 +38,25 @@ public class SendEmailServiceTest {
 
     @MockBean
     private JavaMailSender mailSender;
+    //private GreenMailBean greenMailBean;
 
-    @Container
-    static GenericContainer greenMailGenericContainer = new GenericContainer<>(DockerImageName.parse("greenmail/standalone:latest"))
-            .waitingFor(Wait.forLogMessage(".*Starting GreenMail standalone.*", 1))
-            .withEnv("GREENMAIL_OPTS", "-Dgreenmail.setup.test.smtp -Dgreenmail.hostname=0.0.0.0 -Dgreenmail.users=user:admin")
-            .withExposedPorts(3025);
-
-    @DynamicPropertySource
-    static void configureMailHost(DynamicPropertyRegistry registry) {
-        registry.add("spring.mail.host", greenMailGenericContainer::getHost);
-        registry.add("spring.mail.port", () -> greenMailGenericContainer.getMappedPort(3025));
-    }
-
-    @RegisterExtension
-    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
-            .withConfiguration(GreenMailConfiguration.aConfig().withUser("nao.responda425@gmail.com", "fdswibvggzvdebsl"))
-            .withPerMethodLifecycle(false);
+//    @BeforeEach
+//    void setUp() {
+//        greenMailBean = new GreenMailBean();
+//    }
 
     @Test
     @DisplayName("send email to the provided email")
     void sendEmail_givenEmail_success() {
+        GreenMail greenMail = new GreenMail(ServerSetup.ALL);
+        greenMail.start();
+
         User user = new User("sergio@example.com", "Sergio","password#123");
         userRepository.save(user);
 
         sendEmailService.sendEmail(user.getEmail(), user.getToken());
+
+        Assertions.assertTrue(greenMail.waitForIncomingEmail(5000, 1));
 
         Optional<User> foundUserOptional = userRepository.findByEmail(user.getEmail());
 
@@ -82,5 +72,7 @@ public class SendEmailServiceTest {
         Assertions.assertEquals(user.isValidated(), foundUser.isValidated());
         Assertions.assertEquals(user.getRole(), foundUser.getRole());
         Assertions.assertEquals(user.getName(), foundUser.getName());
+
+        greenMail.stop();
     }
 }
