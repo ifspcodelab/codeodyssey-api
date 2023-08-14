efb44e6d6299422f2d62ca3fb387cec1fec9d11f
kevindotklein <kevindotklein@gmail.com>
2023-08-14 19:01:25 -0300

    refactor: remove unused annotations

--- src/main/java/app/codeodyssey/codeodysseyapi/user/data/User.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/user/data/User.java
@@ -35,7 +35,6 @@ public class User implements UserDetails {
     private boolean isValidated;
     private String token;
 
-    @JsonIgnore
     @OneToMany(mappedBy = "user")
     private List<RefreshToken> refreshTokens;
 
