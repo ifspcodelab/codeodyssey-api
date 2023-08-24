d41ffe080f57f053d8e2c87950b89845402c1914
rafaelspa <rsdepaula67@gmail.com>
2023-08-24 14:10:15 -0300

    feat: add log resend of confirmation email

--- src/main/java/app/codeodyssey/codeodysseyapi/user/service/ResendEmailService.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/user/service/ResendEmailService.java
@@ -7,12 +7,15 @@ import app.codeodyssey.codeodysseyapi.user.api.UserResponse;
 import app.codeodyssey.codeodysseyapi.user.data.User;
 import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ResendEmailService {
     private UserRepository userRepository;
     private final UserMapper userMapper;
@@ -28,7 +31,7 @@ public class ResendEmailService {
         }
 
         sendEmailService.sendEmail(user.getEmail(), user.getToken());
-
+        log.info("Confirmation email was resent to {}", user.getEmail());
         return userMapper.to(user);
     }
 }
