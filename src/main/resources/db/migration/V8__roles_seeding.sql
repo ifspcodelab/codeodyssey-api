85fca7257e7f88cb79ae1ccd323c8fdf242f6bf5
kevindotklein <kevindotklein@gmail.com>
2023-08-24 15:55:13 -0300

    feat: add admin to roles seeding

--- src/main/resources/db/migration/V8__roles_seeding.sql
+++ src/main/resources/db/migration/V8__roles_seeding.sql
@@ -1,3 +1,4 @@
 INSERT INTO roles (id, role) VALUES
     ('4c6ff0d5-f227-451b-9568-a8c391b11850', 'STUDENT'),
-    ('99cede7a-eefd-4a11-95e7-6f73dfd0ece0', 'PROFESSOR')
+    ('99cede7a-eefd-4a11-95e7-6f73dfd0ece0', 'PROFESSOR'),
+    ('ac37e235-4ac6-4bab-8d91-a605dfa325c1', 'ADMIN')
