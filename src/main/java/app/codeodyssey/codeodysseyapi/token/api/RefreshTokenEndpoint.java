f37da89896e5e66750f85ac66ad022d480a6e240
kevindotklein <kevindotklein@gmail.com>
2023-08-24 16:14:39 -0300

    fix: refresh token endpoint docs

--- src/main/java/app/codeodyssey/codeodysseyapi/token/api/RefreshTokenEndpoint.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/token/api/RefreshTokenEndpoint.java
@@ -31,7 +31,7 @@ public class RefreshTokenEndpoint {
 
     @Operation(
             summary = "Use refresh token to generate a new access token",
-            description = "Returns the refresh token and the new access token")
+            description = "Returns a new refresh token and the new access token")
     @ApiResponses({
         @ApiResponse(
                 responseCode = "201",
