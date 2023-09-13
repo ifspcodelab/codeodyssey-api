2d8e0c6aaa47fc01774cf2c8edbdbea1e090a55d
Raquel Tsuji <raqueltsuji@gmail.com>
2023-09-13 19:10:01 -0300

    feat: add exceptions in get invitations service

--- src/main/java/app/codeodyssey/codeodysseyapi/invitation/api/GetInvitationsEndpoint.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/invitation/api/GetInvitationsEndpoint.java
@@ -4,6 +4,8 @@ import app.codeodyssey.codeodysseyapi.invitation.service.GetInvitationsService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,9 @@ public class GetInvitationsEndpoint {
     private final GetInvitationsService getInvitationsService;
 
     @GetMapping("{courseId}")
-    public ResponseEntity<List<InvitationResponse>> get(
-            @PathVariable @Valid UUID courseId) {
-        return ResponseEntity.ok(getInvitationsService.execute(courseId));
+    public ResponseEntity<List<InvitationResponse>> get(@PathVariable @Valid UUID courseId, Authentication authentication) {
+        var userDetails = (UserDetails) authentication.getPrincipal();
+        var username = userDetails.getUsername();
+        return ResponseEntity.ok(getInvitationsService.execute(courseId, username));
     }
 }
--- src/main/java/app/codeodyssey/codeodysseyapi/invitation/service/GetInvitationsService.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/invitation/service/GetInvitationsService.java
@@ -1,21 +1,40 @@
 package app.codeodyssey.codeodysseyapi.invitation.service;
 
+import app.codeodyssey.codeodysseyapi.common.exception.EmailNotFoundException;
+import app.codeodyssey.codeodysseyapi.common.exception.Resource;
+import app.codeodyssey.codeodysseyapi.common.exception.ResourceNotFoundException;
+import app.codeodyssey.codeodysseyapi.course.data.Course;
+import app.codeodyssey.codeodysseyapi.course.data.CourseRepository;
 import app.codeodyssey.codeodysseyapi.invitation.api.InvitationResponse;
 import app.codeodyssey.codeodysseyapi.invitation.data.InvitationRepository;
+import app.codeodyssey.codeodysseyapi.user.data.User;
+import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class GetInvitationsService {
     private final InvitationRepository invitationRepository;
+    private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
     private final InvitationMapper invitationMapper;
 
-    public List<InvitationResponse> execute(UUID courseId) {
+    public List<InvitationResponse> execute(UUID courseId, String userEmail) {
+        Optional<User> user = userRepository.findByEmail(userEmail);
 
-        return invitationMapper.to(invitationRepository.findAllByCourseId(courseId));
+        if (user.isEmpty()) {
+            throw new EmailNotFoundException(userEmail);
+        }
+
+        Course course = courseRepository
+                .findById(courseId)
+                .orElseThrow(() -> new ResourceNotFoundException(courseId, Resource.COURSE));
+
+        return invitationMapper.to(invitationRepository.findAllByCourseId(course.getId()));
     }
 }
