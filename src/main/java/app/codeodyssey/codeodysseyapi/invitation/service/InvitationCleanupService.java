a84c5d83b6168362447ebeb222e966d6d6afa2f5
Raquel Tsuji <raqueltsuji@gmail.com>
2023-10-02 20:07:29 -0300

    feat: add invitation deactivation after end of its course

---
+++ src/main/java/app/codeodyssey/codeodysseyapi/invitation/service/InvitationCleanupService.java
@@ -0,0 +1,35 @@
+package app.codeodyssey.codeodysseyapi.invitation.service;
+
+import app.codeodyssey.codeodysseyapi.invitation.data.Invitation;
+import app.codeodyssey.codeodysseyapi.invitation.data.InvitationRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Log4j2
+public class InvitationCleanupService {
+    private final InvitationRepository invitationRepository;
+
+    @Scheduled(fixedRateString = "${scheduler.invitation.interval}")
+    public void cleanupInvitation (){
+        List<Invitation> invitationWhereCourseEnded = invitationRepository.findByCourseEndDate(LocalDate.now());
+
+        for (Invitation invitation : invitationWhereCourseEnded){
+            if(invitation.isActive()){
+                invitation.setActive(false);
+                invitationRepository.save(invitation);
+
+                log.info("Invitation with id " + invitation.getId() + " set to deactivated due to end of course");
+            }
+        }
+
+        invitationWhereCourseEnded.clear();
+    }
+
+}
