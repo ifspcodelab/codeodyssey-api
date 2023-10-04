31644c96b266e60f05d7e2ff0c39243afa37dcf1
Raquel Tsuji <raqueltsuji@gmail.com>
2023-10-04 17:29:43 -0300

    feat: add activity table

---
+++ src/main/resources/db/migration/V6_create_activities_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE activities(
+    id UUID NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    course_id UUID NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    initial_file BYTEA NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    CONSTRAINT activities_pk PRIMARY KEY (id),
+    CONSTRAINT course_fk FOREIGN KEY (course_id) REFERENCES courses(id)
+)
