302b3c9535977571b169d71b5b67f6e22b603576
Raquel Tsuji <raqueltsuji@gmail.com>
2023-10-04 18:00:16 -0300

    feat: remove unnecessary function

--- src/main/java/app/codeodyssey/codeodysseyapi/resolution/service/CreateResolutionService.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/resolution/service/CreateResolutionService.java
@@ -9,13 +9,11 @@ import app.codeodyssey.codeodysseyapi.course.data.Course;
 import app.codeodyssey.codeodysseyapi.course.data.CourseRepository;
 import app.codeodyssey.codeodysseyapi.resolution.api.ResolutionResponse;
 import app.codeodyssey.codeodysseyapi.resolution.data.Resolution;
-import app.codeodyssey.codeodysseyapi.resolution.data.ResolutionRepository;
 import app.codeodyssey.codeodysseyapi.user.data.User;
 import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,7 +24,6 @@ public class CreateResolutionService {
     private final UserRepository userRepository;
     private final CourseRepository courseRepository;
     private final ActivityRepository activityRepository;
-    private final ResolutionRepository resolutionRepository;
 
     public ResolutionResponse execute(UUID courseId, UUID activityId, CreateResolutionCommand command, String userEmail) {
         Optional<User> user = userRepository.findByEmail(userEmail);
@@ -43,13 +40,7 @@ public class CreateResolutionService {
 
         Optional<Activity> activity = activityRepository.findById(activityId);
 
-        List<Resolution> listResolution = resolutionRepository.findAllByActivityIdAndStudentId(activityId, user.get().getId());
-        if (listResolution.isEmpty()) {
-            Resolution resolution = new Resolution(activity.get(), user.get(), activity.get().getInitialFile());
-            return resolutionMapper.to(resolution);
-        } else {
-            Resolution resolution = new Resolution(activity.get(), user.get(), command.resolutionFile());
-            return resolutionMapper.to(resolution);
-        }
+        Resolution resolution = new Resolution(activity.get(), user.get(), command.resolutionFile());
+        return resolutionMapper.to(resolution);
     }
 }
