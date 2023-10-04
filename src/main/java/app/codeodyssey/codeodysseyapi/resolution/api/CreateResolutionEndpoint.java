e56aa822dcc08a5ab780f7e83690d2bba643f6c4
Raquel Tsuji <raqueltsuji@gmail.com>
2023-10-04 17:35:38 -0300

    feat: add create resolution endpoint

---
+++ src/main/java/app/codeodyssey/codeodysseyapi/resolution/api/CreateResolutionEndpoint.java
@@ -0,0 +1,28 @@
+package app.codeodyssey.codeodysseyapi.resolution.api;
+
+import app.codeodyssey.codeodysseyapi.resolution.service.CreateResolutionCommand;
+import app.codeodyssey.codeodysseyapi.resolution.service.CreateResolutionService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/courses/{courseId}/activities/{activityId}/resolutions")
+@AllArgsConstructor
+public class CreateResolutionEndpoint {
+    private final CreateResolutionService createResolutionService;
+
+    @PostMapping
+    public ResponseEntity<ResolutionResponse> post(
+            @PathVariable UUID courseId, @PathVariable UUID activityId, @Valid @RequestBody CreateResolutionCommand command, Authentication authentication) {
+        var userDetails = (UserDetails) authentication.getPrincipal();
+        var username = userDetails.getUsername();
+        return new ResponseEntity<>(createResolutionService.execute(courseId, activityId, command, username), HttpStatus.CREATED);
+    }
+}
