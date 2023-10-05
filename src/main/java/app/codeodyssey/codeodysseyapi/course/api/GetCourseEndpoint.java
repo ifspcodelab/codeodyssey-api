1b7ed29a531f0f0e178e82247fd40e943bce67d7
kevindotklein <kevindotklein@gmail.com>
2023-10-05 18:09:26 -0300

    docs: get course endpoint

--- src/main/java/app/codeodyssey/codeodysseyapi/course/api/GetCourseEndpoint.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/course/api/GetCourseEndpoint.java
@@ -1,8 +1,15 @@
 package app.codeodyssey.codeodysseyapi.course.api;
 
 import app.codeodyssey.codeodysseyapi.course.service.GetCourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +22,29 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/courses")
 @RequiredArgsConstructor
+@Tag(name = "Courses", description = "Course API endpoint")
 public class GetCourseEndpoint {
 
     private final GetCourseService getCourseService;
 
+    @Operation(
+            summary = "Get a course.",
+            description = "Returns a course based on its id.",
+            tags = {"Courses"})
+            @ApiResponses({
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            schema = @Schema(implementation = CourseResponse.class),
+                                            mediaType = "application/json")
+                            }),
+            @ApiResponse(
+                    responseCode = "403",
+                    content = {
+                            @Content(schema = @Schema(implementation = ProblemDetail.class), mediaType = "application/json")
+                    })
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CourseResponse> get(@PathVariable UUID id, Authentication authentication) {
         return new ResponseEntity<>(this.getCourseService.execute(id, authentication), HttpStatus.OK);
