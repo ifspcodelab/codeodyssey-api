fd8342050ad3bb0dbaa0740242402643cacd2fe2
Raquel Tsuji <raqueltsuji@gmail.com>
2023-10-11 17:18:20 -0300

    feat: add access to professor in get activity service

--- src/main/java/app/codeodyssey/codeodysseyapi/activity/service/GetActivityService.java
+++ src/main/java/app/codeodyssey/codeodysseyapi/activity/service/GetActivityService.java
@@ -3,9 +3,11 @@ package app.codeodyssey.codeodysseyapi.activity.service;
 import app.codeodyssey.codeodysseyapi.activity.api.ActivityResponse;
 import app.codeodyssey.codeodysseyapi.activity.data.ActivityRepository;
 import app.codeodyssey.codeodysseyapi.common.exception.*;
+import app.codeodyssey.codeodysseyapi.course.data.CourseRepository;
 import app.codeodyssey.codeodysseyapi.enrollment.data.EnrollmentRepository;
 import app.codeodyssey.codeodysseyapi.user.data.User;
 import app.codeodyssey.codeodysseyapi.user.data.UserRepository;
+import app.codeodyssey.codeodysseyapi.user.data.UserRole;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +20,7 @@ public class GetActivityService {
     private final ActivityMapper activityMapper;
     private final ActivityRepository activityRepository;
     private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
     private final EnrollmentRepository enrollmentRepository;
 
     public ActivityResponse execute(UUID courseId, UUID activityId, String userEmail) {
@@ -31,8 +34,16 @@ public class GetActivityService {
             throw new ViolationException(Resource.ACTIVITY, ViolationType.ACTIVITY_IS_NOT_FROM_COURSE, activityId.toString());
         }
 
-        if (!enrollmentRepository.existsByStudentIdAndInvitation_Course_Id(user.get().getId(), courseId)) {
-            throw new StudentNotEnrolledException(user.get().getId(), courseId);
+        if (user.get().getRole().equals(UserRole.STUDENT)){
+            if (!enrollmentRepository.existsByStudentIdAndInvitation_Course_Id(user.get().getId(), courseId)) {
+                throw new StudentNotEnrolledException(user.get().getId(), courseId);
+            }
+        }
+
+        if (user.get().getRole().equals(UserRole.PROFESSOR)){
+            if (!courseRepository.existsByProfessorIdAndId(user.get().getId(), courseId)) {
+                throw new StudentNotEnrolledException(user.get().getId(), courseId);
+            }
         }
 
         return activityMapper.to(activityRepository.findByCourseIdAndActivityId(courseId, activityId));
